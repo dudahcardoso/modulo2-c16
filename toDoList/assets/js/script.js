bd14e571279bc8b5389d28e256324719eee1331c
dudahcardoso <mcardoso@blueedtech.com.br>
2022-05-23 14:43:23 -0300

    atualizando os comentários no código

--- toDoList/assets/js/script.js
+++ toDoList/assets/js/script.js
@@ -1,44 +1,49 @@
-const btnAdd = document.querySelector("#addBtn");
+const btnAdd = document.querySelector("#addBtn");//pega o botão no DOM
 
+//Cria um evento de click para o botão e executa a function anônima abaixo
 btnAdd.addEventListener("click", function (){
     const li = document.createElement("li");//criou elemento novo li
-    const inputValue = document.querySelector("#myInput").value;
-    const taskText = document.createTextNode(inputValue);
-    li.appendChild(taskText);
+    const inputValue = document.querySelector("#myInput").value;//pega o texto digitado no input
+    const taskText = document.createTextNode(inputValue);//adiciona o texto da minha tarefa na taskText
+    li.appendChild(taskText);//adiciono o meu texto na li
 
-    //verificação se está embranco o campo
+    //verificação se está em branco o input
     if(inputValue === ""){
         alert("Você deve digitar alguma tarefa.")
     }else{
-        document.getElementById("myUL").appendChild(li);
+        document.getElementById("myUL").appendChild(li);//adiciona a li como filha da ul e ele encaixa a minha li na ul
     }
 
-    //botão fechar
-    const span = document.createElement("span");
-    const icon = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(icon);
-    li.appendChild(span);
-
+    //zerar o valor do input
     document.querySelector("#myInput").value = "";
 
-    //apagar a tarefa
-    const close = document.querySelectorAll(".close");
+    //botão fechar/apagar
+    const span = document.createElement("span");//cria um elemento span
+    const icon = document.createTextNode("\u00D7");
+    span.className = "close";//adiciona um texto no HTML com o ícone de x de fechar
+    span.appendChild(icon);//insere o icon na span
+    li.appendChild(span);//insere a span na li criada
 
+    //apagar a tarefa definitivamente
+    const close = document.querySelectorAll(".close");//pega todas as spans de fechar
+
+    //varre a lista de spans e faz a verificação uma por uma, e vê e qual foi a clicada
     for(let i = 0; i < close.length; i++){
         close[i].addEventListener("click", function (){
-            const li = this.parentElement;
-            li.style.display ="none";
+            const li = this.parentElement;//pega o parente e o nesse caso é element é a li
+            li.style.display ="none";//muda o display para none fazendo ele sumir da tela
         });
-    };   
-
+    };  
+    
+});//termina a função anonima
 
 //check na tarefa
-    const list = document.querySelector("ul");
+    const list = document.querySelector("ul");//captura a lista inteira, vai pegar a ul inteira
 
+    //adicionando um evento de click
     list.addEventListener("click", (ev) => {
+        //dentro da minha target tudo que tiver o nome nodeName==LI
         if(ev.target.nodeName === "LI"){
-            ev.target.classList.toggle("checked");
+            ev.target.classList.toggle("checked");//vai ser adicionado como checked
         }
-    })
-});
+    });
