d6ff12fb7b985e0c76ed5a5956c124d38f7d6ea3
dudahcardoso <mcardoso@blueedtech.com.br>
2022-06-08 20:05:46 -0300

    subindo as paginas de EJS

--- aula16/src/controllers/FilmeControllers.js
+++ aula16/src/controllers/FilmeControllers.js
@@ -1,6 +1,7 @@
 const Filme = require("../models/Filmes");//trazendo o filme que está cadastrado os dados na tabela no meu banco de dados
 let message = "";
 
+//getAll - Lista todos
 const getAll = async (req, res) => {//async, aguarda porque eu vou lá fazer a consulta espere as ações para renderizar a página
     try{//tente por esse caminho se der certo
         const filmes = await Filme.findAll();//aguardando
@@ -15,6 +16,8 @@ const getAll = async (req, res) => {//async, aguarda porque eu vou lá fazer a c
     };
 };
 
+
 module.exports = {
-    getAll
+    getAll,
+    getById
 }
